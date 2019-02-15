71da3f922504135a47dfc7b5cddd68150dbdbd4f
trevorengstrom <32318456+trevorengstrom@users.noreply.github.com>
2019-02-15 14:01:36 -0800

    index page to add to Database

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Add to Guestbook </title>
+	<style >
+	
+	</style>
+</head>
+<body>
+<h2> Sign the guestbook </h2>
+
+<form action="submit.php" method="post">
+	First Name:<br>
+	<input type="text" name="firstname" >
+	<br>
+	Last Name: <br>
+	<input type="text" name="lastname" >
+	<br><br>
+	<input type="submit" value="Submit">
+</form>
+
+
+</body>
+
+</html>
